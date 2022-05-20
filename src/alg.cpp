a9e1a3c8f77ffe3a5e81e53a14f3b52a230b59a0
ust-tanya <94698360+ust-tanya@users.noreply.github.com>
2022-05-20 22:35:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,24 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2022 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int right = size - 1;
+  int left = 0, mid = 0;
+  while (left <= right) {
+  mid = (left + right) / 2;
+  if (value == *(arr + mid)) {
+  left = right = mid;
+  while (*(arr + right) == *(arr + right + 1)) {
+  right++;
+  }
+  while (*(arr + left) == *(arr + left - 1)) {
+  left--;
+  }
+  return right - left + 1;
+  } else if (value > *(arr + mid)) {
+  left = mid + 1;
+  } else if (value < *(arr + mid)) {
+  right = mid - 1;
+  }
+  }
+  return 0;
 }
